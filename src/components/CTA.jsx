1e34101cea3e719bb601296e78a128aa0186fba1
Leo-Cata <63213371+Leo-Cata@users.noreply.github.com>
2023-03-12 20:58:09 -0300

    CTA responsive

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,22 +3,22 @@ import Buttons from './Buttons';
 
 const CTA = () => {
   return (
-    <div className="bg-mainCyan flex justify-evenly items-center py-20 ">
-      <div className="flex flex-col text-5xl text-mainFont">
-        <span className="bg-white rounded-xl w-fit py-4 px-8 my-4">
+    <div className="bg-mainCyan flex justify-evenly items-center py-10 lg:py-20 flex-col lg:flex-row ">
+      <div className="flex flex-col text-3xl md:text-5xl text-mainFont max-w-[90%] sm:max-w-none">
+        <span className="bg-white rounded-xl w-fit py-2 sm:py-4 px-2 sm:px-8 my-4">
           Thinking About
         </span>
-        <span className="bg-mainYellow rounded-xl w-fit py-4 px-8 font-bold">
+        <span className="bg-mainYellow rounded-xl w-fit py-2 sm:py-4 px-2 sm:px-8 font-bold text-center">
           Languague Learning?
         </span>
       </div>
-      <div className="w-1/3 text-lg">
-        <p>
+      <div className="w-3/4 md:w-1/3 md:text-lg space-y-8 my-4">
+        <p className="">
           Learn a language right here with hundreds of free language-learning
           lessons. Get access to compact lessons from the experts and connect
           with a community of native speakers to help you master words faster.
         </p>
-        <div className="flex">
+        <div className="flex justify-center md:justify-start">
           <Buttons />
         </div>
       </div>
