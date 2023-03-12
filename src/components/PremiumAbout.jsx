e42c5550563b6988f6a433720a9592b7b36bba6c
Leo-Cata <63213371+Leo-Cata@users.noreply.github.com>
2023-03-12 19:13:10 -0300

    premiun about responsive

--- src/components/PremiumAbout.jsx
+++ src/components/PremiumAbout.jsx
@@ -2,24 +2,28 @@ import React from 'react';
 import { person2 } from '../assets';
 const PremiumAbout = () => {
   return (
-    <div className="flex justify-center w-full space-x-10 my-32">
+    <div className="flex justify-center w-full space-y-8 lg:space-y-0 lg:space-x-10 my-32 lg:flex-row flex-col">
       {/* title, text and button */}
-      <div className="w-1/3 self-center space-y-10">
-        <h1 className="text-mainFont font-bold text-5xl mb-5">
+      <div className="lg:w-1/4 w-3/4 self-center space-y-10">
+        <h1 className="text-mainFont font-bold text-3xl lg:text-5xl mb-5">
           Boost your learning with{' '}
           <span className="text-mainCyan">Dialect</span> plus
         </h1>
-        <p className="text-secondaryFont text-lg">
+        <p className="text-secondaryFont lg:text-lg">
           Learning with us is fun, and research shows that it works! Learning a
           language on Dialect is completely free, but you can remove ads and
           make faster progress with Plus. Try it free for 14 days!
         </p>
-        <button className="bg-mainYellow text-xl px-9 py-4 text-mainFont">
-          Start Learning
-        </button>
+        <div className="flex justify-center lg:block lg:justify-start">
+          <button className="bg-mainYellow text-xl px-9 py-4 text-mainFont rounded-md justify-self-center">
+            Start Learning
+          </button>
+        </div>
       </div>
       {/* image */}
-      <img src={person2} alt="person reading illustration" />
+      <div className="lg:max-0 mx-auto">
+        <img src={person2} alt="person reading illustration" />
+      </div>
     </div>
   );
 };
