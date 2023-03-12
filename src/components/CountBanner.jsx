9ab826ced233a02bd961a9be0b063f316e818a05
Leo-Cata <63213371+Leo-Cata@users.noreply.github.com>
2023-03-12 19:17:26 -0300

    banner responsive

--- src/components/CountBanner.jsx
+++ src/components/CountBanner.jsx
@@ -3,7 +3,7 @@ import { char1, char2 } from '../assets';
 
 const CountBanner = () => {
   return (
-    <div className="text-mainFont font-bold text-4xl flex justify-evenly items-center bg-mainSalmon py-24">
+    <div className="text-mainFont font-bold text-2xl md:text-3xl xl:text-4xl flex justify-evenly items-center bg-mainSalmon py-8 xl:py-24 flex-col xl:flex-row space-y-8 xl:space-y-0">
       <img src={char1} alt="character A" />
       <div>70,000 lessons taught</div>
       <div>40,000+ students enrolled</div>
