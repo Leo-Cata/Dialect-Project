01c542ee9e39ee4ca79b6413c88234cfe3edcfdd
Leo-Cata <63213371+Leo-Cata@users.noreply.github.com>
2023-03-14 16:31:16 -0300

    learning button fixed

--- src/components/LearningButton.jsx
+++ src/components/LearningButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LearningButton = () => {
   return (
     <div>
-      <button className="bg-mainFont rounded-md py-[15px] px-[35px] text-white">
+      <button className="bg-mainFont rounded-md py-1 sm:py-4 px-4 sm:px-9 text-white">
         Start Learning
       </button>
     </div>
