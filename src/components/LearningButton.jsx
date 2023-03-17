51d2cfe0b6ec148ca3f66b95e1ffe079239a458f
Leo-Cata <63213371+Leo-Cata@users.noreply.github.com>
2023-03-17 17:05:38 -0300

    back to normal

--- src/components/LearningButton.jsx
+++ src/components/LearningButton.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LearningButton = () => {
   return (
     <div>
-      <button className="bg-mainFont rounded-md py-4 px-4 sm:px-9 text-white transition duration-200 hover:scale-105">
+      <button className="bg-mainFont rounded-md py-4 px-4 sm:px-9 text-white transition-all duration-200 hover:scale-105">
         Start Learning
       </button>
     </div>
